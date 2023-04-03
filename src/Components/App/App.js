176cddc8f68ea966e93eb61dd60a55935a3b4c8b
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-04-03 16:24:47 +0630

    done to 45 project file , second commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import PlayList from '../Playlist/Playlist';
+
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults:[{name:'name1',artist:'artist1',album:'album1',id:1},
+      {name:'name2',artist:'artist2',album:'album2',id:2},{name:'name3',artist:'artist3',album:'album3',id:3}],
+      playlistName:'My PlayList',
+      playlistTracks: [{name:'playlistName1', artist:'playlistArtist1', album: 'playlistAlbum1',id:4},
+      {name:'playlistName2', artist:'playlistArtist2', album: 'playlistAlbum2',id:5},
+      {name:'playlistName3', artist:'playlistArtist3', album: 'playlistAlbum3',id:6}]
+    }
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if(tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    }
+    tracks.push(track);
+    this.setState({playlistTracks:tracks});
+    
+  }
+
+  render() {
+    return (
+      <div>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+    <SearchBar/>
+    <div className="App-playlist">
+      <SearchResults searchResults = {this.state.searchResults}
+      onAdd = {this.addTrack}/>
+      <PlayList playlistName = {this.state.playlistName}
+      playlistTracks = {this.state.playlistTracks}/>
+    </div>
+  </div>
+</div>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Playlist.css';
+import TrackList from '../TrackList/TrackList';
+
+class Playlist extends React.Component {
+    render(){
+        return(<div className="Playlist">
+        <input defaultValue = {'New Playlist'}/>
+        <TrackList tracks = {this.props.playlistTracks}/>
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>)
+    }
+}
+export default Playlist;
